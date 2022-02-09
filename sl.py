645ba17a5e6959ca41dda7b8bb2292957094f942
Falcom4000 <a1332291383@163.com>
2022-02-09 23:01:00 +0800

    这是给GUI和录音准备的

--- sl.py
+++ sl.py
@@ -4,7 +4,8 @@ import pandas as pd
 def register_item(dir_name, results):
     if results:
         if results[0]['score'] > 0.6:
-            logid = dir_name[5:-1]
+            logid = dir_name[7:-1]
             result = results[0]['keyword']
             df = pd.read_excel('answer.xlsx')
-            df = df.appdend({'logid': logid, 'result': result})
+            df = df.append({'logid': logid, 'result': result}, ignore_index=True)
+            print(df)
